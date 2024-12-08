16c0cfa8dda93d326ecaafa8487b93f89e28866e
1shaker <salmazini@students.iugaza.edu.ps>
2024-12-08 12:49:23 +0200

    DB-

---
+++ app/Listeners/SendEmailVerification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewUserCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Mail\SendMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmailVerification
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(NewUserCreated $event): void
+    {
+//        sleep(5);
+        //    $event->user->email
+        Mail::to($event->user->email)->send(new SendMail($event->user));
+
+    }
+}
