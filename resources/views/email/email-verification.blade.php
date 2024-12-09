c6beb68d3b5973787ccf0560c216a7a88ed33848
1shaker <salmazini@students.iugaza.edu.ps>
2024-12-09 10:37:19 +0200

    DB-

--- resources/views/email/email-verification.blade.php
+++ resources/views/email/email-verification.blade.php
@@ -7,20 +7,5 @@
 </head>
 <body>
 
-<div class="container" >
-    <div class="card card-body" style="margin-left: 40px;margin-left: 50px;">
-        <div align="center">
-            <a href="{{url('/')}}"><img width="100" src="{{url('/others/logo.png')}}"></a>
-        </div>
-        <h3>E-mail verification.</h3>
-        <p>Hello, <br/>  <br/>
-            we have sent to you this email to check if this Email : <a href="#">{{$user->email}}</a> your provide is a valid one; click on the link to verifiy it.
-
-            <a style="font-weight:bold;color:blue" target="_blank" href="http://127.0.0.1:8000/check_email/{{$user->remember_token}}">Check my email</a>  </p><br/>
-        <p>Yours sincerely.</p>
-    </div>
-
-</div>
-
 </body>
 </html>
