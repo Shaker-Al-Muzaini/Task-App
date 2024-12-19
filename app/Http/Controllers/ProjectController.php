0bac4534043c0ed4dc88b12e940f886d3ea69e6c
1shaker <salmazini@students.iugaza.edu.ps>
2024-12-19 10:29:09 +0200

    Project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -12,14 +12,12 @@ use Illuminate\Http\JsonResponse;
 class ProjectController extends Controller
 {
     /**
-     * تخزين مشروع جديد.
-     *
+
      * @param Request $request
      * @return JsonResponse
      */
     public function store(Request $request): JsonResponse
     {
-        // التحقق من صحة البيانات الواردة
         try {
             $validatedData = $request->validate([
                 'name' => 'required|string|max:255',
@@ -27,7 +25,6 @@ class ProjectController extends Controller
                 'endDate' => 'required|date|after_or_equal:startDate',
             ]);
         } catch (ValidationException $e) {
-            // إرجاع الأخطاء إذا فشل التحقق
             return response()->json(['errors' => $e->errors()], 422);
         }
 
@@ -35,7 +32,6 @@ class ProjectController extends Controller
         DB::beginTransaction();
 
         try {
-            // إنشاء المشروع باستخدام البيانات التي تم التحقق منها
             $project = Project::create([
                 'name' => $validatedData['name'],
                 'status' => Project::NOT_STARTED,
@@ -44,22 +40,18 @@ class ProjectController extends Controller
                 'slug' => Project::createSlug($validatedData['name']),
             ]);
 
-            // تأكيد المعاملة
             DB::commit();
 
             // تحويل المشروع إلى مصفوفة باستخدام Resource
             $projectData = (new ProjectResource($project))->toArray($request);
 
-            // دمج البيانات مع الرسالة في الجذر
             return response()->json(
                 array_merge(['message' => 'تم إنشاء المشروع بنجاح!'], $projectData),
                 201
             );
         } catch (\Exception $e) {
-            // التراجع عن المعاملة إذا حدث خطأ
             DB::rollBack();
 
-            // إرجاع استجابة خطأ
             return response()->json(
                 ['message' => 'حدث خطأ أثناء إنشاء المشروع.', 'error' => $e->getMessage()],
                 500
